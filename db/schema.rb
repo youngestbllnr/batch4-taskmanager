4e62c98b7cd2104fa5bbc17eb8d574f2bdbfc9a4
Carl Rosales <rosales.carlangel@gmail.com>
2021-03-01 20:09:07 +0800

    rearranged migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_28_055144) do
+ActiveRecord::Schema.define(version: 2021_03_01_182608) do
 
   create_table "categories", force: :cascade do |t|
     t.integer "user_id"
