8903968455bfbdf4ab41bc89507abbe7c1dd37a0
Carl Rosales <rosales.carlangel@gmail.com>
2021-03-03 06:12:12 +0800

    run test on main controller

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class MainControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @task = tasks(:one)
+  end
+
+  test "should toggle task is_checked_field" do
+    get toggle_task_url(task_id: @task.id)
+    assert_response :redirect
+  end
 end
