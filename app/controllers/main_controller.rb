aa130eacf336e78ca1cb51bdb316d8b4c9c115ee
Carl Rosales <rosales.carlangel@gmail.com>
2021-03-03 05:44:13 +0800

    accomplished MVP, deploy to heroku

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,11 @@
 class MainController < ApplicationController
-    before_action :authenticate_user!, only: [ :dashboard ]
+    before_action :authenticate_user!, only: [ :dashboard, :today, :toggle_task ]
 
     def index
+        if current_user.present?
+            redirect_to dashboard_path
+        end
+        session.delete(:dev)
     end
 
     def dashboard
@@ -9,9 +13,29 @@ class MainController < ApplicationController
     end
 
     def dev
+        session[:dev] = 1;
     end
 
     def today
+        @checked_today = []
+        @unchecked_today = []
+        categories = current_user.categories
+        categories.each do |category|
+            category.tasks.each do |task|
+                @checked_today.push(task) if task.due_date == Date.today && task.is_checked
+                @unchecked_today.push(task) if task.due_date == Date.today && !task.is_checked
+            end
+        end
+    end
+
+    def toggle_task
+        task = Task.find(params[:task_id])
+        if task.is_checked
+            task.toggle!(:is_checked)
+        else
+            task.toggle!(:is_checked)
+        end
+        redirect_to category_path(task.category, anchor: "task_#{ task.id }")
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'today' => 'main#today', as: 'today'
 
+  get 'toggle-task' => 'main#toggle_task', as: 'toggle_task'
+
   devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_up => "signup" }
 
   devise_scope :user do
