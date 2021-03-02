3f1ebc36c29169cb4b66a576a88623b3956580f4
Carl Rosales <rosales.carlangel@gmail.com>
2021-03-02 19:40:45 +0800

    performed tests on model Category

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,29 +1,40 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
+
+  test "should not save when icon is blank" do
+    category = Category.new(icon: "", title: "Sample Category", description: "This is a sample category.")
+    assert_not category.save
+  end
+
+  test "should not save when icon is not present" do
+    category = Category.new(title: "Sample Category", description: "This is a sample category.")
+    assert_not category.save
+  end
   
   test "should not save when title is blank" do
-    category = Category.new(title: "", description: "This is a sample category.")
+    category = Category.new(icon: "🔖", title: "", description: "This is a sample category.")
     assert_not category.save
   end
 
   test "should not save when title is not present" do
-    category = Category.new(description: "This is a sample category.")
+    category = Category.new(icon: "🔖", description: "This is a sample category.")
     assert_not category.save
   end
 
   test "should not save when description is blank" do
-    category = Category.new(title: "Sample Category", description: "")
+    category = Category.new(icon: "🔖", title: "Sample Category", description: "")
     assert_not category.save
   end
 
   test "should not save when description is not present" do
-    category = Category.new(title: "Sample Category")
+    category = Category.new(icon: "🔖", title: "Sample Category")
     assert_not category.save
   end
 
   test "should save when everything is awesome" do
-    category = Category.new(title: "Sample Category", description: "This is a sample category.")
+    category = Category.new(icon: "🔖", title: "Sample Category", description: "This is a sample category.")
+    byebug
     assert category.save
   end
 
