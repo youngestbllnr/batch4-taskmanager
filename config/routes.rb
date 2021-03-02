931a4003d00190479789e7033425ae7b7c17f564
Carl Rosales <rosales.carlangel@gmail.com>
2021-03-02 19:39:43 +0800

    added main#today to routes as today

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   get 'dashboard' => 'main#dashboard', as: 'dashboard'
   get 'dev-mode' => 'main#dev', as: 'dev'
 
+  get 'today' => 'main#today', as: 'today'
 
-  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "signup" }
+  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_up => "signup" }
 
   devise_scope :user do
+    get 'signout'    => 'devise/sessions#destroy'
     get 'forgot-password'    => 'devise/passwords#new'
     get 'change-password'    => 'devise/passwords#edit'
   end
