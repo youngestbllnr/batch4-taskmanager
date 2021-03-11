3af37e0eb488a4e1426400a0c8e98fd4c1188611
Carl Rosales <rosales.carlangel@gmail.com>
2021-03-11 15:47:44 +0800

    refactored main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,22 +2,18 @@ class MainController < ApplicationController
     before_action :authenticate_user!, except: :index
 
     def index
-        if current_user.present?
-            redirect_to dashboard_path
-        end
-        session.delete(:dev)
+        redirect_to dashboard_path if current_user.present?
     end
 
     def dashboard
-        redirect_to root_url if current_user.nil?
+        redirect_to root_url unless current_user.present?
         @categories = current_user.categories
     end
 
     def today
         @checked_today = []
         @unchecked_today = []
-        categories = current_user.categories
-        categories.each do |category|
+        current_user.categories.each do |category|
             category.tasks.each do |task|
                 @checked_today.push(task) if task.due_date == Date.today && task.is_checked
                 @unchecked_today.push(task) if task.due_date == Date.today && !task.is_checked
