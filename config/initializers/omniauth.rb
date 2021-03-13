bd13590f89982204489722ebf785231cf1f4da5c
Carl Rosales <rosales.carlangel@gmail.com>
2021-03-13 15:53:04 +0800

    deleted duplicate config for omniauth

--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, Rails.application.credentials.facebook[:app_id], Rails.application.credentials.facebook[:app_secret]
-    provider :google_oauth2, Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:client_secret]
-    provider :twitter, Rails.application.credentials.twitter[:api_key], Rails.application.credentials.twitter[:api_secret_key]
-    provider :github, Rails.application.credentials.github[:client_id], Rails.application.credentials.github[:client_secret]
-end
