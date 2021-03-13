0ab8b28777eeb5121abb8ca5c1c9eafd335c1508
Carl Rosales <rosales.carlangel@gmail.com>
2021-03-13 16:07:25 +0800

    configured routes for omniauth callbacks, initialized omniauth callbacks controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,20 @@ Rails.application.routes.draw do
 
   get 'toggle-task' => 'tasks#toggle_status', as: 'toggle_task'
 
-  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_up => "signup" }
+  devise_for :users, :path => '', 
+                     :path_names => { :sign_in => "login", :sign_up => "signup" }, 
+                     :controllers => { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   devise_scope :user do
     get 'signout'    => 'devise/sessions#destroy'
     get 'forgot-password'    => 'devise/passwords#new'
     get 'change-password'    => 'devise/passwords#edit'
   end
+
+  match 'auth/facebook/callback' => redirect('users/omniauth_callbacks')
+  match 'auth/google/callback' => redirect('users/omniauth_callbacks')
+  match 'auth/twitter/callback' => redirect('users/omniauth_callbacks')
+  match 'auth/github/callback' => redirect('users/omniauth_callbacks')
   
   resources :categories do
     resources :tasks
