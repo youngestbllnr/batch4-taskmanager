7e831a6b62ccaddb28ba714a4c62f0037f176012
Carl Rosales <rosales.carlangel@gmail.com>
2021-03-13 16:11:54 +0800

    updated user model, added from_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
 
   validates :firstname, allow_blank: false, presence: true
   validates :lastname, allow_blank: false, presence: true
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
