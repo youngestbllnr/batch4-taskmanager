d2aba89fe8b966ba08a7565a57318d25d46bf9ca
Carl Rosales <rosales.carlangel@gmail.com>
2021-03-13 13:25:28 +0800

    updated user (devise) model, made it omniauthbale, added omniauth providers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_many :categories, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[ facebook google twitter github ]
 
   validates :firstname, allow_blank: false, presence: true
   validates :lastname, allow_blank: false, presence: true
