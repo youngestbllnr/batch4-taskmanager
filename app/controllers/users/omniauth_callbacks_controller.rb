36b6ade7d3b85533e2d65d7ca12e0b3af8206eb7
Carl Rosales <rosales.carlangel@gmail.com>
2021-03-13 16:26:01 +0800

    configured routes for omniauth callbacks, initialized omniauth callbacks controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  skip_before_action :verify_authenticity_token, only: :facebook
+  skip_before_action :verify_authenticity_token, only: :auth
 
-  def facebook
+  def auth
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
