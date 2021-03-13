7af7cc0ae399a76eb9017542ba0fd196c7041c45
Carl Rosales <rosales.carlangel@gmail.com>
2021-03-13 16:13:44 +0800

    omniauth callback configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,12 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0, 20]
     end
   end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.oauth_data"] && session["devise.oauth_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 end
