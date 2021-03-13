a6089002c7f7d86c5639d17841106823721c3c37
Carl Rosales <rosales.carlangel@gmail.com>
2021-03-13 14:16:54 +0800

    added omniauth fields on User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_073053) do
+ActiveRecord::Schema.define(version: 2021_03_13_052112) do
 
   create_table "categories", force: :cascade do |t|
     t.integer "user_id"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2021_03_11_073053) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "firstname"
     t.string "lastname"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
