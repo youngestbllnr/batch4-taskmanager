d944147a39933738a326a331c7062c68582ae146
Carl Rosales <rosales.carlangel@gmail.com>
2021-03-13 16:35:23 +0800

    refactored line #13 for consistency

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :path => '', 
                      :path_names => { :sign_in => "login", :sign_up => "signup" }, 
-                     :controllers => { omniauth_callbacks: 'users/omniauth_callbacks' }
+                     :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
 
   devise_scope :user do
     get 'signout'    => 'devise/sessions#destroy'
